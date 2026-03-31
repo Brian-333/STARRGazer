ce637c8bbec501c161a492f57b849da208d42f82
Brian Lin <shihhsu1@ualbert.ca>
2026-03-31 15:49:28 -0600

    feat: average fps, parameters

--- src/CustomTracker/yolo.py
+++ src/CustomTracker/yolo.py
@@ -12,6 +12,8 @@ class YOLOTracker:
         # Open the webcam (0 is usually the default camera)
         cap = cv2.VideoCapture(video_source)
 
+        past_10_fps = []
+
         last_time = time.time()
         while cap.isOpened():
             success, frame = cap.read()
@@ -22,12 +24,26 @@ class YOLOTracker:
             results = self.model.predict(frame, device=device, stream=True, show_conf=True)
 
             # Visualize the results on the frame
-            annotated_frame = results[0].plot()
+            annotated_frame = frame.copy()
+            for result in results:
+                boxes = result.boxes.xyxy.cpu().numpy()  # Bounding box coordinates
+                confidences = result.boxes.conf.cpu().numpy()  # Confidence scores
+                class_ids = result.boxes.cls.cpu().numpy()  # Class IDs
+
+                for box, conf, cls_id in zip(boxes, confidences, class_ids):
+                    x1, y1, x2, y2 = map(int, box)
+                    label = f'{self.model.names[int(cls_id)]}: {conf:.2f}'
+                    cv2.rectangle(annotated_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    cv2.putText(annotated_frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
             time_diff = time.time() - last_time
             last_time = time.time()
 
-            annotated_frame = cv2.putText(annotated_frame, f'FPS: {1/time_diff:.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            past_10_fps.append(1/time_diff)
+            if len(past_10_fps) > 10:
+                past_10_fps.pop(0)
+
+            annotated_frame = cv2.putText(annotated_frame, f'FPS: {sum(past_10_fps)/len(past_10_fps):.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
             # Display the annotated frame
             cv2.imshow("YOLOv11 Webcam Inference", annotated_frame)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
 import CustomTracker.yolo
 from pathlib import Path
+import argparse
 
 SRC_DIR = Path(__file__).parent.resolve()
 MODEL_PATH = SRC_DIR / 'best.pt'
 
 def main():
-    new_tracker = CustomTracker.yolo.YOLOTracker(MODEL_PATH)
-    result = new_tracker.predict()
+    args = argparse.ArgumentParser(description="Rocket Tracker")
+    args.add_argument('--device', type=str, default='cuda:0', help='Device to run the model on (e.g., "cuda:0", "cpu" or "mps")')
+    args.add_argument('--video_source', type=int, default=0, help='Video source (default is 0 for webcam)')
+    args.add_argument('--model_path', type=str, default=MODEL_PATH, help='Path to the YOLO model weights')
+    args = args.parse_args()
+
+    new_tracker = CustomTracker.yolo.YOLOTracker(args.model_path)
+    result = new_tracker.predict(device=args.device)
 
 
 if __name__ == "__main__":
